62ee990ae09a783ce3c8c406a98b214261dd50c2
Migelle Mertens <migelle.mertens@gmail.com>
2021-03-16 16:31:07 +0100

    les bankrekening

---
+++ Les - Bankrekening klasse/Bankrekening.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les___Bankrekening_klasse
+{
+    class Bankrekening
+    {
+        private double huidigSaldo;
+        private string mMunteenheid;
+
+        public Bankrekening()
+        {
+            this.huidigSaldo = 0;
+            this.mMunteenheid = "Eur";
+        }
+
+        public Bankrekening(string munt)
+        {
+            this.huidigSaldo = 0;
+            this.mMunteenheid = munt;
+        }
+
+        public double HuidigSaldo
+        {
+            get { return huidigSaldo; }
+            set { huidigSaldo = value; }
+        }
+
+        public string  Munteenheid
+        {
+            get { return mMunteenheid; }
+            set { mMunteenheid = value; }
+        }
+
+        public void BedragWijzigen(double wijziging)
+        {
+            this.huidigSaldo += wijziging;
+        }
+
+    }
+}
---
+++ Les - Bankrekening klasse/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Les___Bankrekening_klasse
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Bankrekening mijnBankrekening;
+        private Bankrekening nieuweBankrekening;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mijnBankrekening = new Bankrekening();
+            mijnBankrekening.Munteenheid = "US $";
+            lblMunteenheid.Content = $"Stand rekening in {mijnBankrekening.Munteenheid}:";
+            txtStand.Text = $"{mijnBankrekening.HuidigSaldo:0.00}";
+
+            Bankrekening uwBankrekening = new Bankrekening("Eur");
+            uwBankrekening.HuidigSaldo = 1202.7;
+            MessageBox.Show($"uw bankrekeningsaldo: {uwBankrekening.HuidigSaldo}");
+
+            Bankrekening onzeBankrekening = new Bankrekening("US $");
+            onzeBankrekening.HuidigSaldo = 740.68;
+            MessageBox.Show($"uw bankrekeningsaldo: {onzeBankrekening.HuidigSaldo}");
+        }
+
+        private void btnWisselMuntEenheid_Click(object sender, RoutedEventArgs e)
+        {
+            if(mijnBankrekening.Munteenheid == "Eur")
+            {
+                mijnBankrekening.Munteenheid = "US $";
+            }
+            else
+            {
+                mijnBankrekening.Munteenheid = "Eur";
+            }
+            lblMunteenheid.Content = $"Stand rekening in {mijnBankrekening.Munteenheid}:";
+        }
+
+        private void btnTonen_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Munt: {nieuweBankrekening.Munteenheid} - saldo: {nieuweBankrekening.HuidigSaldo}");
+        }
+
+        private void txtBedrag_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Enter)
+            {
+                if(txtBedrag.Text != "")
+                {
+                    mijnBankrekening.BedragWijzigen(Convert.ToDouble(txtBedrag.Text));
+                    txtStand.Text = $"{mijnBankrekening.HuidigSaldo:0.00}";
+                    txtBedrag.Clear();
+                }
+            }
+        }
+    }
+}
