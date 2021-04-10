d5b1954efe9540685cd2ba089f2bc0af58074625
Migelle Mertens <migelle.mertens@gmail.com>
2021-04-10 21:05:49 +0200

    global update

---
+++ Dev/Blad Steen Schaar UI/ButtonTestWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Blad_Steen_Schaar_UI
+{
+    /// <summary>
+    /// Interaction logic for ButtonTestWindow.xaml
+    /// </summary>
+    public partial class ButtonTestWindow : Window
+    {
+       
+        public ButtonTestWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBlad_MouseEnter(object sender, MouseEventArgs e)
+        {
+            btnBladImage.Source = new BitmapImage(new Uri(@"img/blad_hover.png", UriKind.RelativeOrAbsolute));            
+        }
+
+        private void btnBlad_Click(object sender, RoutedEventArgs e)
+        {
+            btnBlad.BorderBrush = new SolidColorBrush(Colors.Red);
+            btnBlad.BorderThickness = new Thickness(2, 2, 2, 2);
+        }
+
+        private void btnBlad_MouseLeave(object sender, MouseEventArgs e)
+        {
+            btnBladImage.Source = new BitmapImage(new Uri(@"img/blad.png", UriKind.RelativeOrAbsolute));
+        }
+    }
+}
---
+++ Dev/Timer pause/Timer pause/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Timer_pause
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer pauze = new DispatcherTimer();
+        private int counter = 3;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            pauze.Interval = TimeSpan.FromMilliseconds(1000);
+            pauze.Tick += CountdownPauze;
+        }
+
+        private void CountdownPauze(object sender, EventArgs e)
+        {
+            counter--;
+            if(counter == 0)
+            {
+                pauze.Stop();
+                counter = 3;
+                textLabel.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void pauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            textLabel.Visibility = Visibility.Visible;
+            pauze.Start();
+        }
+    }
+}
